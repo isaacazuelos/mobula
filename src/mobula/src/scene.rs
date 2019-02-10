ee189a615b6a9eb1c0bba952746e3105fc81277c
Isaac Azuelos <isaac@azuelos.ca>
2019-02-09 22:30:21 -0700

    Parellelism!

--- src/mobula/src/scene.rs
+++ src/mobula/src/scene.rs
@@ -1,10 +1,9 @@
+use image::{ImageBuffer, Rgb};
+use indicatif;
+use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
 
-use image::{ImageBuffer, Rgb};
-
-use indicatif;
-
-use crate::camera::CameraBuilder;
+use crate::camera::{CameraBuilder, Camera};
 use crate::config::Config;
 use crate::hit::{Hit, Hitable};
 use crate::material::Scatter;
@@ -57,31 +56,57 @@ impl Scene {
         hit
     }
 
-    pub fn render(&self) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
-        let camera = self.camera.build(&self.config);
+    pub fn render_par(&self) -> Vec<(u8, u8, u8)> {
         let width = self.config.width;
         let height = self.config.height;
-        let pb = indicatif::ProgressBar::new((width * height) as u64);
+        let camera = self.camera.build(&self.config);
 
-        let img = ImageBuffer::from_fn(width, height, |i, j| {
-            pb.inc(1);
+        let progress_bar = indicatif::ProgressBar::new((width * height) as u64);
 
-            let u = (i as f64) / (width as f64);
-            let v = ((height - j) as f64) / (height as f64);
+        let bytes = (0..(self.config.width * self.config.height))
+            .into_par_iter()
+            .inspect(|_| progress_bar.inc(1))
+            .map(|i| {
+                let x = i % width;
+                let y = (i - x) / width;
+                self.render_pixel(x, y, &camera)
+            })
+            .collect();
 
-            let mut c = V3::zero();
-            for _ in 0..self.config.samples {
-                let h_sample = rand::random::<f64>() / (width as f64);
-                let v_sample = rand::random::<f64>() / (height as f64);
-                let ray = camera.get_ray(u + h_sample, v + v_sample);
-                c = c + self.colour(ray, 0);
-            }
-            c = c * (1.0 / (self.config.samples as f64));
-            c = V3::new(c.x.sqrt(), c.y.sqrt(), c.z.sqrt());
-            as_pixel(c)
-        });
-        pb.finish_with_message("complete!");
-        img
+        progress_bar.finish_with_message("complete!");
+        bytes
+    }
+
+    pub fn render_pixel(&self, i: u32, j: u32, camera: &Camera) -> (u8, u8, u8) {
+        let width = self.config.width;
+        let height = self.config.height;
+
+        let u = (i as f64) / (width as f64);
+        let v = ((height - j) as f64) / (height as f64);
+
+        let mut c = V3::zero();
+        for _ in 0..self.config.samples {
+            let h_sample = rand::random::<f64>() / (width as f64);
+            let v_sample = rand::random::<f64>() / (height as f64);
+            let ray = camera.get_ray(u + h_sample, v + v_sample);
+            c = c + self.colour(ray, 0);
+        }
+        c = c * (1.0 / (self.config.samples as f64));
+        c = V3::new(c.x.sqrt(), c.y.sqrt(), c.z.sqrt());
+
+        let ir = (255.99 * c.x) as u8;
+        let ig = (255.99 * c.y) as u8;
+        let ib = (255.99 * c.z) as u8;
+        (ir, ig, ib)
+    }
+
+    pub fn render(&self) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+        let buf = self.render_par();
+
+        ImageBuffer::from_fn(self.config.width, self.config.height, |x, y| {
+            let pixel_index: usize = ((y * self.config.width) + x) as usize;
+            as_pixel(buf[pixel_index])
+        })
     }
 
     fn colour(&self, ray: Ray, depth: u32) -> V3 {
@@ -108,11 +133,8 @@ fn linear_interpolation(start: V3, end: V3, t: f64) -> V3 {
     (start * (1.0 - t)) + (end * t)
 }
 
-fn as_pixel(colour: V3) -> image::Rgb<u8> {
-    let ir = (255.99 * colour.x) as u8;
-    let ig = (255.99 * colour.y) as u8;
-    let ib = (255.99 * colour.z) as u8;
-    image::Rgb([ir, ig, ib])
+fn as_pixel((r, g, b): (u8, u8, u8)) -> image::Rgb<u8> {
+    image::Rgb([r, g, b])
 }
 
 fn colour_background(ray: Ray) -> V3 {
